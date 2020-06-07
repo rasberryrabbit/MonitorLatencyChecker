c7b34c7cb99628043bce861071f655a25608fb0f
rasberryrabbit <lovelust@hanmail.net>
2020-06-07 13:14:17 +0900

    Change to interactive design.

--- HID_promicro/HID_promicro.ino
+++ HID_promicro/HID_promicro.ino
@@ -7,73 +7,59 @@
  *  
  */
 
-//#define USE_HID
-
 const int pinLed = LED_BUILTIN; //
-uint8_t rawhidData[64];
+uint8_t rawData[12];
 uint8_t obuf[64];
 
 int photo=8;
 int flag=0;
 unsigned long st,et;
 int old=LOW;
+int mx=0;
+int my=0;
 
 void setup() {
   pinMode(photo, INPUT);
   Serial.begin(115200);
-#ifdef USE_HID
-  RawHID.begin(rawhidData, sizeof(rawhidData));
-#endif
+  Mouse.begin();
 }
 
 void loop() {
   if(flag==0) {
     if(Serial.available()) {
-      Serial.readBytes(rawhidData,3);
-      st=micros();
-      if(rawhidData[0]=='B' && rawhidData[1]=='G') {
-        if(rawhidData[2]=='0') {
-          old=0;
-        } else if(rawhidData[2]=='1') {
-          old=1;
-        } else {
-          old=digitalRead(photo);
+      Serial.readBytes(rawData,2);
+      if(rawData[0]=='M') {
+        switch(rawData[1]) {
+          case 'X': 
+              Serial.readBytes(&rawData[2],5);
+              rawData[7]=0;
+              mx=atoi(&rawData[2]);
+              Serial.println(mx);
+              break;
+          case 'Y': 
+              Serial.readBytes(&rawData[2],5);
+              rawData[7]=0;
+              my=atoi(&rawData[2]);
+              Serial.println(my);
+              break;
+          case 'T': 
+              Mouse.move(mx,my);
+              delay(100);
+              flag=1;
+              old=digitalRead(photo);
+              Mouse.click();
+              st=micros();
+              break;
+          default:
+              Serial.println("OK");
         }
-        flag=1;
       } else {
-        // serial print
-        Serial.write("OK\n");
-#ifdef USE_HID
-        RawHID.write("OK");
-#endif
+        Serial.println("OK");
       }
       // remove unused chars
       while(Serial.available())
         Serial.read();
     }
-#ifdef USE_HID
-    else
-    if(RawHID.available()) {
-      RawHID.readBytes(rawhidData,64);
-      st=micros();
-      if(rawhidData[0]=='B' && rawhidData[1]=='G') {
-        if(rawhidData[2]=='0') {
-          old=0;
-        } else if(rawhidData[2]=='1') {
-          old=1;
-        } else {
-          old=digitalRead(photo);
-        }
-        flag=1;
-      } else {
-        RawHID.write("OK");
-        Serial.write("OK\n");
-      }
-      // remove unused chars
-      while(RawHID.available())
-        RawHID.read();
-    }
-#endif
   } else {
     if(digitalRead(photo)!=old) {
       et=micros();
@@ -84,9 +70,6 @@ void loop() {
         obuf[0]='T';
       }
       snprintf(&obuf[1],64,"%012ld",et-st);
-#ifdef USE_HID
-      RawHID.write(obuf,64);
-#endif
       // serial print
       Serial.write(obuf,64);
       Serial.println("");
@@ -94,9 +77,5 @@ void loop() {
     // skip input chars on waiting
     while(Serial.available())
       Serial.read();
-#ifdef USE_HID
-    while(RawHID.available())
-      RawHID.read();
-#endif
   }
 }
