7dfbd600ec8c2915ef3bf0def7f456542298e514
rasberryrabbit <lovelust@hanmail.net>
2020-06-08 10:59:32 +0900

    Back to old method.

--- HID_promicro/HID_promicro.ino
+++ HID_promicro/HID_promicro.ino
@@ -1,8 +1,3 @@
-#include <PinChangeInterrupt.h>
-#include <PinChangeInterruptBoards.h>
-#include <PinChangeInterruptPins.h>
-#include <PinChangeInterruptSettings.h>
-
 #include <HID-Project.h>
 #include <HID-Settings.h>
 
@@ -13,7 +8,7 @@
  */
 
 const int pinLed = LED_BUILTIN; //
-uint8_t rawData[12];
+uint8_t rawData[32];
 uint8_t obuf[64];
 
 int photo=8;
@@ -22,13 +17,11 @@ unsigned long st,et;
 int old=LOW;
 int mx=0;
 int my=0;
-int changed=0;
 
 void setup() {
   pinMode(photo, INPUT);
   Serial.begin(115200);
   Mouse.begin();
-  attachPCINT(digitalPinToPCINT(photo), pinchanged, CHANGE);
 }
 
 void loop() {
@@ -50,10 +43,9 @@ void loop() {
           case 'T': 
               Mouse.move(mx,my);
               old=digitalRead(photo);
-              changed=0;
               flag=1;
-              st=micros();
               Mouse.click();
+              st=micros();
               break;
           default:
               Serial.println("OK");
@@ -66,28 +58,22 @@ void loop() {
         Serial.read();
     }
   } else {
-    if(changed==1) {
+    if(digitalRead(photo)!=old) {
+      et=micros();
       flag=0;
+      old=digitalRead(photo);
       if(old) {
         obuf[0]='F';
       } else {
         obuf[0]='T';
       }
       snprintf(&obuf[1],64,"%012ld",et-st);
+      st=et;
       // serial print
       Serial.write(obuf,64);
-      Serial.println("");
     }
     // skip input chars on waiting
     while(Serial.available())
       Serial.read();
-  }
-}
-
-void pinchanged(void) {
-  if(flag) {
-    et=micros();
-    changed=1;
-    old=digitalRead(photo);
   }
 }
