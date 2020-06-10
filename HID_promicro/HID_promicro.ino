07e3c19a21103af6470ca1fd3492bb343be81006
rasberryrabbit <lovelust@hanmail.net>
2020-06-10 09:18:59 +0900

    Implement trigger mode.

--- HID_promicro/HID_promicro.ino
+++ HID_promicro/HID_promicro.ino
@@ -3,7 +3,8 @@
 
 /* 
  *  firmware for arduino pro micro
- *  8pin = photodiode input.
+ *  pin 8 = photodiode input.
+ *  pin 9 = toggle trigger
  *  
  */
 
@@ -16,40 +17,44 @@ int trig=9;
 int flag=0;
 unsigned long st,et;
 int old=LOW;
+int tr_enable=0;
+int trigger_delay=200;
+unsigned long lt,ct;
 
 void setup() {
   pinMode(photo, INPUT);
   pinMode(trig, INPUT_PULLUP);
   Serial.begin(115200);
   Mouse.begin();
+  lt=millis();
 }
 
 void loop() {
-  if(flag==0) {
-    // pin 9 manual trigger
-    if(digitalRead(trig)==0) {
-      old=digitalRead(photo);
-      flag=1;
-      Mouse.click();
-      st=micros();
-      Serial.println("Trigger");
-    } else
-    // serial trigger
-    if(Serial.available()) {
-      Serial.readBytes(rawData,2);
-      if(rawData[0]=='M' && rawData[1]=='T') {
-        old=digitalRead(photo);
+  // toggle trigger
+  if(digitalRead(trig)==0) {
+    tr_enable=!tr_enable;
+    // reset wait condition
+    if(!tr_enable) {
+      flag=0;
+    }
+  }
+  if(!flag) {
+    // start trigger
+    if(tr_enable) {
+      ct=millis();
+      if(ct-lt>=trigger_delay) {
+        lt=ct;
         flag=1;
+        old=digitalRead(photo);
         Mouse.click();
         st=micros();
-      } else {
-        Serial.println("OK");
       }
-      // remove unused chars
-      while(Serial.available())
-        Serial.read();
+    } 
+    while(Serial.available()) {
+      Serial.read();
     }
   } else {
+    // wait changes
     if(digitalRead(photo)!=old) {
       et=micros();
       flag=0;
@@ -64,8 +69,5 @@ void loop() {
       // serial print
       Serial.write(obuf,64);
     }
-    // skip input chars on waiting
-    while(Serial.available())
-      Serial.read();
   }
 }
